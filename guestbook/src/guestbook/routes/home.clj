61c0504f01295b36e87df6e8590b16686cad7340
manmyung <manmyung@gmail.com>
2016-02-11 17:45:46 +0900

    Until 'Defining HTTP Routes'

--- guestbook/src/guestbook/routes/home.clj
+++ guestbook/src/guestbook/routes/home.clj
@@ -1,17 +1,24 @@
 (ns guestbook.routes.home
   (:require [guestbook.layout :as layout]
-            [compojure.core :refer [defroutes GET]]
-            [ring.util.http-response :refer [ok]]
-            [clojure.java.io :as io]))
+            [compojure.core :refer [defroutes GET POST]]
+            [guestbook.db.core :as db]
+            [ring.util.response :as response]))
 
 (defn home-page []
   (layout/render
-    "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
+    "home.html"
+    {:messages (db/get-messages)}))
+
+(defn save-message! [{:keys [params]}]
+  (db/save-message!
+    (assoc params :timestamp (java.util.Date.)))
+  (response/redirect "/"))
 
 (defn about-page []
   (layout/render "about.html"))
 
 (defroutes home-routes
   (GET "/" [] (home-page))
+  (POST "/message" request (save-message! request))
   (GET "/about" [] (about-page)))
 
