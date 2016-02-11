f03617cf1b2d18a137c7907017855ae9d17e7a42
manmyung <manmyung@gmail.com>
2016-02-11 18:02:07 +0900

    Until 'Validating Input'

--- guestbook/src/guestbook/routes/home.clj
+++ guestbook/src/guestbook/routes/home.clj
@@ -1,24 +1,37 @@
 (ns guestbook.routes.home
-  (:require [guestbook.layout :as layout]
+  (:require [bouncer.core :as b]
+            [bouncer.validators :as v]
+            [guestbook.layout :as layout]
             [compojure.core :refer [defroutes GET POST]]
             [guestbook.db.core :as db]
             [ring.util.response :as response]))
 
-(defn home-page []
+(defn home-page [{:keys [flash]}]
   (layout/render
     "home.html"
-    {:messages (db/get-messages)}))
+    (merge {:messages (db/get-messages)}
+           (select-keys flash [:name :message :errors]))))
+
+(defn validate-message [params]
+  (first
+    (b/validate
+      params
+      :name v/required
+      :message [v/required [v/min-count 10]])))
 
 (defn save-message! [{:keys [params]}]
-  (db/save-message!
-    (assoc params :timestamp (java.util.Date.)))
-  (response/redirect "/"))
+  (if-let [errors (validate-message params)]
+    (-> (response/redirect "/")
+        (assoc :flash (assoc params :errors errors)))
+    (do
+      (db/save-message!
+        (assoc params :timestamp (java.util.Date.))) (response/redirect "/"))))
 
 (defn about-page []
   (layout/render "about.html"))
 
 (defroutes home-routes
-  (GET "/" [] (home-page))
+  (GET "/" request (home-page request))
   (POST "/message" request (save-message! request))
   (GET "/about" [] (about-page)))
 
