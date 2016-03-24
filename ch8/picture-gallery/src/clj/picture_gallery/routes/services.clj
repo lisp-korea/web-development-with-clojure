96de897ecbf5547a4f0eeeb7ce6d98a2ded83c60
manmyung <manmyung@gmail.com>
2016-03-24 16:16:28 +0900

    Add Authentication Routes

--- ch8/picture-gallery/src/clj/picture_gallery/routes/services.clj
+++ ch8/picture-gallery/src/clj/picture_gallery/routes/services.clj
@@ -1,73 +1,26 @@
 (ns picture-gallery.routes.services
-  (:require [ring.util.http-response :refer :all]
+  (:require [picture-gallery.routes.services.auth :as auth]
+            [ring.util.http-response :refer :all]
             [compojure.api.sweet :refer :all]
             [schema.core :as s]))
 
-(s/defschema Thingie {:id Long
-                      :hot Boolean
-                      :tag (s/enum :kikka :kukka)
-                      :chief [{:name String
-                               :type #{{:id String}}}]})
+(s/defschema UserRegistration
+  {:id                     String
+   :pass                   String
+   :pass-confirm           String})
+
+(s/defschema Result
+  {:result                   s/Keyword
+   (s/optional-key :message) String})
 
 (defapi service-routes
-  {:swagger {:ui "/swagger-ui"
-             :spec "/swagger.json"
-             :data {:info {:version "1.0.0"
-                           :title "Sample API"
-                           :description "Sample Services"}}}}
-  (context "/api" []
-    :tags ["thingie"]
-
-    (GET "/plus" []
-      :return       Long
-      :query-params [x :- Long, {y :- Long 1}]
-      :summary      "x+y with query-parameters. y defaults to 1."
-      (ok (+ x y)))
-
-    (POST "/minus" []
-      :return      Long
-      :body-params [x :- Long, y :- Long]
-      :summary     "x-y with body-parameters."
-      (ok (- x y)))
-
-    (GET "/times/:x/:y" []
-      :return      Long
-      :path-params [x :- Long, y :- Long]
-      :summary     "x*y with path-parameters"
-      (ok (* x y)))
-
-    (POST "/divide" []
-      :return      Double
-      :form-params [x :- Long, y :- Long]
-      :summary     "x/y with form-parameters"
-      (ok (/ x y)))
-
-    (GET "/power" []
-      :return      Long
-      :header-params [x :- Long, y :- Long]
-      :summary     "x^y with header-parameters"
-      (ok (long (Math/pow x y))))
-
-    (PUT "/echo" []
-      :return   [{:hot Boolean}]
-      :body     [body [{:hot Boolean}]]
-      :summary  "echoes a vector of anonymous hotties"
-      (ok body))
-
-    (POST "/echo" []
-      :return   (s/maybe Thingie)
-      :body     [thingie (s/maybe Thingie)]
-      :summary  "echoes a Thingie from json-body"
-      (ok thingie)))
-
-  (context "/context" []
-    :tags ["context"]
-    :summary "summary inherited from context"
-    (context "/:kikka" []
-      :path-params [kikka :- s/Str]
-      :query-params [kukka :- s/Str]
-      (GET "/:kakka" []
-        :path-params [kakka :- s/Str]
-        (ok {:kikka kikka
-             :kukka kukka
-             :kakka kakka})))))
+        {:swagger {:ui "/swagger-ui"
+                   :spec "/swagger.json"
+                   :data {:info {:version "1.0.0"
+                                 :title "Picture Gallery API"
+                                 :description "Public Services"}}}}
+        (POST "/register" req
+              :return Result
+              :body [user UserRegistration]
+              :summary "register a new user"
+              (auth/register! req user)))
