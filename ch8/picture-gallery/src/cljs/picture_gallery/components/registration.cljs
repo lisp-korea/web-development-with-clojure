148b298eeecbf2fd0db1c352c823f0942686cc71
manmyung <manmyung@gmail.com>
2016-03-25 07:08:16 +0900

    Register the user from our form

--- ch8/picture-gallery/src/cljs/picture_gallery/components/registration.cljs
+++ ch8/picture-gallery/src/cljs/picture_gallery/components/registration.cljs
@@ -1,9 +1,27 @@
 (ns picture-gallery.components.registration
   (:require [reagent.core :refer [atom]]
-            [picture-gallery.components.common :as c]))
+            [picture-gallery.components.common :as c]
+            [ajax.core :as ajax]
+            [reagent.session :as session]
+            [picture-gallery.validation :refer [registration-errors]]))
+
+(defn register! [fields errors]
+  (reset! errors (registration-errors @fields))
+  (when-not @errors
+    (ajax/POST "/register"
+               {:params @fields
+                :handler
+                        #(do
+                          (session/put! :identity (:id @fields))
+                          (reset! fields {}))
+                :error-handler
+                        #(reset!
+                          errors
+                          {:server-error (get-in % [:response :message])})})))
 
 (defn registration-form []
-  (let [fields (atom {})]
+  (let [fields (atom {})
+        error (atom nil)]
     (fn []
       [c/modal
        [:div "Picture Gallery Registration"]
@@ -12,7 +30,11 @@
          [:strong "✱ required field"]]
         [c/text-input "name" :id "enter a user name" fields]
         [c/password-input "password" :pass "enter a password" fields]
-        [c/password-input "password" :pass-confirm "re-enter the password" fields]]
+        [c/password-input "password" :pass-confirm "re-enter the password" fields]
+        (when-let [error (:server-error @error)]
+          [:div.alert.alert-danger error])]
        [:div
-        [:button.btn.btn-primary "Register"]
+        [:button.btn.btn-primary
+         {:on-click #(register! fields error)}
+         "Register"]
         [:button.btn.btn-danger "Cancel"]]])))
