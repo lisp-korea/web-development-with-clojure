b6dd14314be05fc5d6c6d0fcd5ef8319501ba8bf
manmyung <manmyung@gmail.com>
2016-03-25 07:53:52 +0900

    Register the User

--- ch8/picture-gallery/src/cljs/picture_gallery/components/registration.cljs
+++ ch8/picture-gallery/src/cljs/picture_gallery/components/registration.cljs
@@ -1,8 +1,8 @@
 (ns picture-gallery.components.registration
   (:require [reagent.core :refer [atom]]
+            [reagent.session :as session]
             [picture-gallery.components.common :as c]
             [ajax.core :as ajax]
-            [reagent.session :as session]
             [picture-gallery.validation :refer [registration-errors]]))
 
 (defn register! [fields errors]
@@ -13,7 +13,8 @@
                 :handler
                         #(do
                           (session/put! :identity (:id @fields))
-                          (reset! fields {}))
+                          (reset! fields {})
+                          (session/remove! :modal))
                 :error-handler
                         #(reset!
                           errors
@@ -21,7 +22,7 @@
 
 (defn registration-form []
   (let [fields (atom {})
-        error (atom nil)]
+        error  (atom nil)]
     (fn []
       [c/modal
        [:div "Picture Gallery Registration"]
@@ -29,7 +30,11 @@
         [:div.well.well-sm
          [:strong "✱ required field"]]
         [c/text-input "name" :id "enter a user name" fields]
+        (when-let [error (first (:id @error))]
+          [:div.alert.alert-danger error])
         [c/password-input "password" :pass "enter a password" fields]
+        (when-let [error (first (:pass @error))]
+          [:div.alert.alert-danger error])
         [c/password-input "password" :pass-confirm "re-enter the password" fields]
         (when-let [error (:server-error @error)]
           [:div.alert.alert-danger error])]
@@ -37,4 +42,11 @@
         [:button.btn.btn-primary
          {:on-click #(register! fields error)}
          "Register"]
-        [:button.btn.btn-danger "Cancel"]]])))
+        [:button.btn.btn-danger
+         {:on-click #(session/remove! :modal)}
+         "Cancel"]]])))
+
+(defn registration-button []
+  [:a.btn
+   {:on-click #(session/put! :modal registration-form)}
+   "register"])
--- ch8/picture-gallery/src/cljs/picture_gallery/core.cljs
+++ ch8/picture-gallery/src/cljs/picture_gallery/core.cljs
@@ -17,6 +17,16 @@
     {:href uri
      :on-click #(reset! collapsed? true)} title]])
 
+(defn user-menu []
+  (if-let [id (session/get :identity)]
+    [:ul.nav.navbar-nav.pull-xs-right
+     [:li.nav-item
+      [:a.dropdown-item.btn
+       {:on-click #(session/remove! :identity)}
+       [:i.fa.fa-user] " " id " | sign out"]]]
+    [:ul.nav.navbar-nav.pull-xs-right
+     [:li.nav-item [reg/registration-button]]]))
+
 (defn navbar []
   (let [collapsed? (r/atom true)]
     (fn []
@@ -28,7 +38,8 @@
         [:a.navbar-brand {:href "#/"} "picture-gallery"]
         [:ul.nav.navbar-nav
          [nav-link "#/" "Home" :home collapsed?]
-         [nav-link "#/about" "About" :about collapsed?]]]])))
+         [nav-link "#/about" "About" :about collapsed?]]]
+       [user-menu]])))
 
 (defn about-page []
   [:div.container
@@ -55,9 +66,13 @@
   {:home #'home-page
    :about #'about-page})
 
+(defn modal []
+  (when-let [session-modal (session/get :modal)]
+    [session-modal]))
+
 (defn page []
   [:div
-   [reg/registration-form]
+   [modal]
    [(pages (session/get :page))]])
 
 ;; -------------------------
