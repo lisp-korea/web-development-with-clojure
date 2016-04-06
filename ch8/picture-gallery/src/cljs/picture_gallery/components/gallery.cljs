960e543a8f60c6e960a16c482aa927ff538208d2
manmyung <manmyung@gmail.com>
2016-04-06 17:42:28 +0900

    UI for Deleting Pictures

--- ch8/picture-gallery/src/cljs/picture_gallery/components/gallery.cljs
+++ ch8/picture-gallery/src/cljs/picture_gallery/components/gallery.cljs
@@ -13,14 +13,45 @@
        :src link}]
      [:div.modal-backdrop.fade.in]]))
 
+(defn delete-image! [name]
+  (ajax/DELETE (str "/image/" name)
+               {:handler #(do
+                           (session/update-in!
+                             [:thumbnail-links]
+                             (fn [links]
+                               (remove
+                                 (fn [link] (= name (:name link)))
+                                 links)))
+                           (session/remove! :modal))}))
+
+(defn delete-image-button [owner name]
+  (session/put!
+    :modal
+    (fn []
+      [c/modal
+       [:h2 "Remove " name "?"]
+       [:div [:img {:src (str "/gallery/" owner "/" name)}]]
+       [:div
+        [:button.btn.btn-primary
+         {:on-click #(delete-image! name)}
+         "delete"]
+        [:button.btn.btn-danger
+         {:on-click #(session/remove! :modal)}
+         "Cancel"]]])))
+
 (defn thumb-link [{:keys [owner name]}]
-  [:div.col-sm-4>img
-   {:src      (str js/context "/gallery/" owner "/" name)
-    :on-click #(session/put!
-                :modal
-                (image-modal
-                  (str js/context "/gallery/" owner "/"
-                       (s/replace name #"thumb_" ""))))}])
+  [:div.col-sm-4
+   [:img
+    {:src      (str js/context "/gallery/" owner "/" name)
+     :on-click #(session/put!
+                 :modal
+                 (image-modal (str js/context
+                                   "/gallery/" owner "/"
+                                   (s/replace name #"thumb_" ""))))}]
+   (when (= (session/get :identity) owner)
+     [:div.text-xs-center>div.btn.btn-danger
+      {:on-click #(delete-image-button owner name)}
+      [:i.fa.fa-times]])])
 
 (defn gallery [links]
   [:div.text-xs-center
