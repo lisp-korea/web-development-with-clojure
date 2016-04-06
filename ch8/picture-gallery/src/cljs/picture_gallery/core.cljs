1d4aa3f529a57228cc081aba24baa65e59e3eb99
manmyung <manmyung@gmail.com>
2016-04-06 18:21:34 +0900

    UI for Account Deletion

--- ch8/picture-gallery/src/cljs/picture_gallery/core.cljs
+++ ch8/picture-gallery/src/cljs/picture_gallery/core.cljs
@@ -20,16 +20,35 @@
     {:href uri
      :on-click #(reset! collapsed? true)} title]])
 
+(defn account-actions [id]
+  (let [expanded? (r/atom false)]
+    (fn []
+      [:div.dropdown
+       {:class    (when @expanded? "open")
+        :on-click #(swap! expanded? not)}
+       [:button.btn.btn-secondary.dropdown-toggle
+        {:type :button}
+        [:span.glyphicon.glyphicon-user] " " id [:span.caret]]
+       [:div.dropdown-menu.user-actions
+        [:a.dropdown-item.btn
+         {:on-click
+          #(session/put!
+            :modal reg/delete-account-modal)}
+         "delete account"]
+        [:a.dropdown-item.btn
+         {:on-click
+          #(ajax/POST
+            "/logout"
+            {:handler (fn [] (session/remove! :identity))})}
+         "sign out"]]])))
+
 (defn user-menu []
   (if-let [id (session/get :identity)]
     [:ul.nav.navbar-nav.pull-xs-right
-     [:li.nav-item [u/upload-button]]
      [:li.nav-item
-      [:a.dropdown-item.btn
-       {:on-click #(ajax/POST
-                    "/logout"
-                    {:handler (fn [] (session/remove! :identity))})}
-       [:i.fa.fa-user] " " id " | sign out"]]]
+      [u/upload-button]]
+     [:li.nav-item
+      [account-actions id]]]
     [:ul.nav.navbar-nav.pull-xs-right
      [:li.nav-item [l/login-button]]
      [:li.nav-item [reg/registration-button]]]))
